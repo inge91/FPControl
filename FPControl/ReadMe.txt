8de4a4bb5676e993a82bfeb1dd4b40ab027bed84
Inge Becht <inge.becht91@gmail.com>
2013-01-16 19:42:10 +0100

    Camera rotation works
    Need to be able to rotate local axis of the camera

---
+++ FPControl/ReadMe.txt
@@ -0,0 +1,35 @@
+========================================================================
+    APPLICATION : FPControl Project Overview
+========================================================================
+
+AppWizard has created this FPControl Application for you.  
+
+This file contains a summary of what you will find in each of the files that
+make up your FPControl application.
+
+FPControl.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard. 
+    It contains information about the version of Visual C++ that generated the file, and 
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+FPControl.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+FPControl.cpp
+    This is the main application source file.
+
+AssemblyInfo.cpp
+	Contains custom attributes for modifying assembly metadata.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ FPControl/main.cpp
@@ -0,0 +1,116 @@
+#include "StdAfx.h"
+#include <iostream>
+#include <stdlib.h>
+#include <GL/glut.h>
+#include <windows.h>
+
+using namespace std;
+
+GLfloat boxsize = 2;
+bool rotating = false;
+GLfloat degrees = 1;
+GLfloat position = 0;
+
+void handleKeypress(unsigned char key, int x, int y) {
+	switch (key) {
+	case 97:
+		degrees -= 5;
+		break;
+	case 100:
+		degrees += 5;
+		break;
+	case 119:
+		position += 1;
+		break;
+	case 115:
+		position -= 1;
+		break;
+	case 27: //Escape key
+			exit(0);
+	}
+}
+
+void initRendering() {
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_NORMALIZE);
+	glEnable(GL_COLOR_MATERIAL);
+
+}
+
+void handleResize(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (float)w / (float)h, 1.0, 200.0);
+}
+
+
+void drawScene() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTranslatef(0, 0, position);
+	glRotatef(degrees, 0, 1, 0);
+	glTranslatef(0, 0, -position);
+
+
+	// glTranslatef(0, 0, -position);
+	glBegin(GL_QUADS);
+	// Cube around camera
+	// Front
+	glColor3f(1, 0, 0);
+	glNormal3f(0, 0, 1);
+	glVertex3f(-20, -20, 20 );
+	glVertex3f( 20, -20, 20 );
+	glVertex3f( 20,  20, 20 );
+	glVertex3f(-20,  20, 20 );
+	
+	// Back
+	glNormal3f(0, 0, 1);
+	glColor3f(0, 1, 0);
+	glVertex3f(-20, -20, -20 );
+	glVertex3f( 20, -20, -20 );
+	glVertex3f( 20,  20, -20 );
+	glVertex3f(-20,  20, -20 );
+
+	// Left 
+	glColor3f(0, 0, 1);
+	glNormal3f(1, 0, 0);
+	glVertex3f(-20, -20, -20 );
+	glVertex3f(-20,  20, -20 );
+	glVertex3f(-20,  20,  20 );
+	glVertex3f(-20,  -20,  20 );
+
+
+	glEnd();
+
+	glutSwapBuffers();
+}
+
+//Called every 25 milliseconds
+void update(int value) {
+	glutPostRedisplay();
+	glutTimerFunc(2, update, 0);
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(800, 600);
+	glutCreateWindow("FPS");
+	//glutFullScreen();
+	initRendering();
+	
+	glutDisplayFunc(drawScene);
+	glutKeyboardFunc(handleKeypress);
+	glutReshapeFunc(handleResize);
+	glutTimerFunc(25, update, 0);
+	
+	glutMainLoop();
+	return 0;
+}
+
