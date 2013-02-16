d7020267398f74b124420ac8081f79bd2a4d0dc9
Inge Becht <inge.becht91@gmail.com>
2013-02-16 19:37:33 +0100

    Signs are drawn nearest to furthes
    Should really fix my circular array and some other issues

--- FPControl/signs.cpp
+++ FPControl/signs.cpp
@@ -76,12 +76,71 @@ GLfloat Signs::calculate_degrees( GLfloat camerax, GLfloat cameraz, GLfloat mpos
 
 }
 
-void Signs::draw_signs(GLfloat camerax, GLfloat cameraz)
+// Check if element is in array
+bool in(int arr[], int element)
 {
 	for(int i = 0; i < 5; i++)
 	{
+		if(arr[i] == element)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void Signs::draw_signs(GLfloat camerax, GLfloat cameraz)
+{
+	
+
+	//Sort the signs on distance to the player
+	int arr[5]= {-1,-1, -1, -1, -1};
+	pair<GLfloat, int> tempd (NULL, NULL);
+	pair<GLfloat, GLfloat> tempp;
+	for(int k = 0; k < 5; k++)
+	{
+		for(int l = 0; l < 5; l++)
+		{
+			// If index already in array skip it
+			if( in(arr, l) )
+			{
+				continue;
+			}
+			else{
+
+				tempp = msigns.get_element(l);
+				GLfloat relposz = cameraz - tempp.second;
+				std::cout<< cameraz<<endl;
+				std::cout<< camerax<<endl;
+				GLfloat relposx = camerax -tempp.first;
+				GLfloat tempdist = sqrt(pow(relposx,2) + pow(relposz,2));
+				// Check if tempd is not yet filled
+				if(tempd.first == NULL)
+				{
+
+				
+					tempd.first = tempdist;
+					tempd.second = l;
+				}
+				else if(tempd.first > tempdist)
+				{
+					tempd.first = tempdist;
+					tempd.second = l;
+				}
+			}
+		}
+		// Fill in array element
+		arr[k] = tempd.second;
+		tempd.first = NULL;
+		tempd.second = NULL;
+	}
+
+	
+	// You have to start at the back
+	for(int i = 4; i > -1 ; i--)
+	{
 		
-		pair<GLfloat, GLfloat> p = msigns.get_element(i);
+		pair<GLfloat, GLfloat> p = msigns.get_element(arr[i]);
 		// temp mposx mposz
 		GLfloat mposx = p.first;
 		GLfloat mposz = p.second;
@@ -102,7 +161,7 @@ void Signs::draw_signs(GLfloat camerax, GLfloat cameraz)
 		glEnable(GL_TEXTURE_2D);
 		glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		glEnable (GL_BLEND);
-		glBindTexture(GL_TEXTURE_2D, mtextures[i]);
+		glBindTexture(GL_TEXTURE_2D, mtextures[arr[i]]);
 		glBegin(GL_QUADS);
 		// Draw the rectangle the alien will be blitted on
 		glTexCoord2f(1, 0); glVertex3f(5,  -20, 0);
